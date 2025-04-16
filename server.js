f7fe8c8bfa2e77633f7d553d45d3a44aee812050
Luana Barassal <luanabarassal123@gmail.com>
2025-04-15 22:30:32 -0300

    corrigido IP para 0.0.0.0 no Render

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+﻿const express = require('express');
 const cors = require('cors');
 const app = express();
 
@@ -13,5 +13,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/viagens', viagensRoutes);
 app.use('/api/despesas', despesasRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+// ✅ ESSA LINHA É A CHAVE PARA FUNCIONAR NO RENDER
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
